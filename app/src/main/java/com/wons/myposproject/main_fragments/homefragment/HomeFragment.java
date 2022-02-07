18edba1a02786bd5fee8f6db3c7ad2adde6bb30d
wono9 <wono96@naver.com>
2022-02-07 14:10:34 +0900

    20220206

--- app/src/main/java/com/wons/myposproject/main_fragments/homefragment/HomeFragment.java
+++ app/src/main/java/com/wons/myposproject/main_fragments/homefragment/HomeFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 
 import com.wons.myposproject.MainActivity;
@@ -35,61 +36,51 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        showDidLog("onCreateView");
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        getCalenderViewDate();
-        getTodayData();
 
+        Format format = new SimpleDateFormat("yyyy / M / d");
+        String date = format.format(binding.calenderView.getDate());
+
+        binding.calenderView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = year + " / " + month + " / " + dayOfMonth;
+
+            }
+        });
         binding.layoutAddSchedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDidLog("layoutAddSchedule + onClick");
-                HomeDialogCallback callback = new HomeDialogCallback() {
+                AlertDialog alertDialog = HomeDialogUtils.getAlertDialogWhenAddedSchedule(getActivity(), new HomeDialogCallback() {
                     @Override
                     public void onResult(Schedule value) {
-                        changeData((Schedule) value, INSERT);
-                        setListView(((Schedule) value).date);
+
                     }
-                };
-                AlertDialog alertDialog = HomeDialogUtils.getAlertDialogWhenAddedSchedule(getActivity(), callback, binding);
+                }, binding);
                 alertDialog.show();
             }
         });
-
-        binding.lvSchedule.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Schedule schedule = (Schedule) parent.getAdapter().getItem(position);
-                HomeDialogCallback callback = new HomeDialogCallback() {
-                    @Override
-                    public void onResult(Schedule value) {
-                        changeData((Schedule) value, DELETE);
-                        setListView(((Schedule) value).date);
-                    }
-                };
-                AlertDialog alertDialog = HomeDialogUtils.getDeleteDialog(getActivity(), callback, schedule);
-                alertDialog.show();
-                return false;
-            }
-        });
-
         return binding.getRoot();
     }
 
-    private static final class SetSchedule {
+
+    private static class SetSchedule {
         static final int CHANGE_CODE_INSERT = 0;
         static final int CHANGE_CODE_DELETE = 1;
+        private static SetSchedule setSchedule = new SetSchedule();
 
-        SetSchedule(String date) {
-            getScheduleOnRoomDB(date);
+        public static void setListView(String date) {
+
+        }
+        public static void changeListView(String date, int code) {
+
         }
 
-        SetSchedule(Schedule schedule, int changeCode) {
-            changeScheduleInDB(schedule, changeCode);
+        private SetSchedule(Schedule schedule, int changeCode) {
         }
 
 
-        static void changeScheduleInDB(Schedule schedule, int changeCode) {
+        private void changeScheduleInDB(Schedule schedule, int changeCode) {
             switch (changeCode) {
                 case CHANGE_CODE_DELETE: {
                     break;
@@ -99,76 +90,13 @@ public class HomeFragment extends Fragment {
                 }
             }
         }
-
-        static void setListView(ArrayList<Schedule> scheduleArrayList) {
-
+        private void setListView(ArrayList<Schedule> scheduleArrayList) {
+            // TODO: 2022-02-07  리스트 뷰의 택스트 날짜 입력
         }
-
-        static void getScheduleOnRoomDB(String date) {
-
-
-//            setListView();
+        private void getScheduleOnRoomDB(String date) {
+            setListView(new ArrayList<>());
         }
-
-    }
-
-
-    private void getCalenderViewDate() {
-        showDidLog("getCalenderViewDate");
-        binding.calenderView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                showDidLog("getCalenderViewDate : onSelectedDayChange");
-                String date = String.valueOf(year) + " / " + (month + 1) + " / " + dayOfMonth;
-                binding.tvScheduleValue.setText(date);
-                setListView(date);
-            }
-        });
-    }
-
-    private void getTodayData() {
-        showDidLog("getTodayData");
-        Format format = new SimpleDateFormat("yyyy / M / d");
-        String date = format.format(binding.calenderView.getDate());
-        binding.tvScheduleValue.setText(date);
-        setListView(date);
-    }
-
-    private Schedule[] getData(String date) {
-        showDidLog("getData");
-        return MainViewModel.getSchedule(getContext(), date);
-    }
-
-    private void changeData(Schedule schedule, int code) {
-        showDidLog("changeData");
-        switch (code) {
-            case DELETE: {
-                showDidLog("changeData+DELETE");
-                MainViewModel.deleteSchedule(getContext(), schedule);
-                break;
-            }
-            case INSERT: {
-                showDidLog("changeData+INSERT");
-                MainViewModel.insertSchedule(getContext(), schedule);
-                break;
-            }
-        }
-        getData(schedule.date);
-    }
-
-    private void setListView(String date) {
-        showDidLog("setListView");
-        if (binding.lvSchedule.getAdapter() == null) {
-            ScheduleAdapter adapter = new ScheduleAdapter();
-            binding.lvSchedule.setAdapter(adapter);
-        }
-        ((ScheduleAdapter) binding.lvSchedule.getAdapter()).setSchedules(new ArrayList<>(Arrays.asList(getData(date))));
-        ((ScheduleAdapter) binding.lvSchedule.getAdapter()).notifyDataSetChanged();
-
-    }
-
-    private void showDidLog(String msg) {
-        Log.d(MainActivity.TAG_DID, TAG + " : " + msg);
+        private SetSchedule() { }
     }
 }
 
