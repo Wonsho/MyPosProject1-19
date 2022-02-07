596a637b9044b00bd4429a42fad79f8354379720
wono9 <wono96@naver.com>
2022-02-07 14:27:40 +0900

    20220206

--- app/src/main/java/com/wons/myposproject/main_fragments/homefragment/HomeFragment.java
+++ app/src/main/java/com/wons/myposproject/main_fragments/homefragment/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.wons.myposproject.main_fragments.homefragment;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -64,37 +65,44 @@ public class HomeFragment extends Fragment {
     }
 
 
+
+
+
     private static class SetSchedule {
         static final int CHANGE_CODE_INSERT = 0;
         static final int CHANGE_CODE_DELETE = 1;
+        private Context context;
         private static SetSchedule setSchedule = new SetSchedule();
 
-        public static void setListView(String date) {
-
+        public void setListView(Context context, String date) {
+            this.context = context;
+            getScheduleOnRoomDB(date);
         }
-        public static void changeListView(String date, int code) {
-
+        public void changeListViewData(Context context, Schedule schedule, int code) {
+            this.context = context;
+            changeScheduleInDB(schedule, code);
         }
-
-        private SetSchedule(Schedule schedule, int changeCode) {
-        }
-
 
         private void changeScheduleInDB(Schedule schedule, int changeCode) {
             switch (changeCode) {
                 case CHANGE_CODE_DELETE: {
+                    MainViewModel.deleteSchedule(context, schedule);
+                    getScheduleOnRoomDB(schedule.date);
                     break;
                 }
                 case CHANGE_CODE_INSERT: {
+                    MainViewModel.insertSchedule(context, schedule);
+                    getScheduleOnRoomDB(schedule.date);
                     break;
                 }
             }
         }
         private void setListView(ArrayList<Schedule> scheduleArrayList) {
-            // TODO: 2022-02-07  리스트 뷰의 택스트 날짜 입력
+
+
         }
         private void getScheduleOnRoomDB(String date) {
-            setListView(new ArrayList<>());
+            setListView(new ArrayList<>(Arrays.asList(MainViewModel.getSchedule(context, date))));
         }
         private SetSchedule() { }
     }
